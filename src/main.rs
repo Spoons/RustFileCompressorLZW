d18beeb9f760433d0bfe526028d0f64fba950d98
miffi <mciocio1@uncc.edu>
2018-04-14 21:29:18 -0400

    slow progress

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use std::error::Error;
 use std::fs::File;
 use std::path::Path;
-use std::str;
 use std::io::{Read, Write};
 use std::collections::HashMap;
+use std::char;
 
 
 
@@ -16,10 +16,25 @@ fn main() {
         Ok(file) => file,
     };
 
-    let mut buf = [0u8; 2048];
+    let mut buf = [0u8; 20];
     let bytes_read = file.read(&mut buf).unwrap();
     println!("b read: {}", bytes_read);
-    println!("{}", str::from_utf8(&buf).unwrap());
+    //println!("{}", str::from_utf8(&buf).unwrap());
 
+    //initialize dictionary with single character values
+    let mut dictionary = HashMap::new();
+    for x in 0..255 {
+        dictionary.insert(vec![x],x as u16);
+    }
 
+    let mut current: Vec<u8> = Vec::new();
+    for x in 0..bytes_read {
+        let mut c = buf[x];
+        current.push(c);
+
+        if dictionary.contains_key(&current) == false {
+            let n = dictionary.len() + 1;
+            dictionary.insert(current.clone(), n as u16);
+        }
+    }
 }
