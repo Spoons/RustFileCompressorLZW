654c0ac0cb12a3ecd09b3a9cc6ce6513897e00d7
miffi <mciocio1@uncc.edu>
2018-04-19 00:22:40 -0400

    writes decoded output to file

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ use std::path::Path;
 use std::str;
 use std::io::{Read, Write};
 use std::collections::HashMap;
+use std::fs::OpenOptions;
+use std::env;
 
 extern crate time;
 use time::PreciseTime;
@@ -22,13 +24,15 @@ fn read_file_to_memory(filename: &str, buffer: &mut Vec<u8>, bytes_read: &mut us
     //create 2mb static buffer
     let mut buf = [0u8; 2097152];
 
+    //set bytes_read pointer to zero
+    *bytes_read = 0;
     loop {
         //this call reads from file and stores contents in buf
         let read_status:Result<_,_> = file.read(&mut buf);
         //here we check if the read was successful by matching
-        //read_states enum with it's states Ok or Err
+        //read_status enum with it's states Ok or Err
         match read_status {
-            //If read_states is Ok then value contains the number
+            //If read_status is Ok then value contains the number
             //of bytes read
             Ok(value) => {
                 //stream is empty so break
@@ -36,7 +40,7 @@ fn read_file_to_memory(filename: &str, buffer: &mut Vec<u8>, bytes_read: &mut us
                     println!("Breaking due to stream being empty.");
                     break;
                 }
-                //write to the bytes_read argument
+                //add num bytes of this read to bytes_read
                 *bytes_read = value;
                 //copy our 2mb buffer into our vector and try reading
                 //from the strem again.
@@ -52,17 +56,37 @@ fn read_file_to_memory(filename: &str, buffer: &mut Vec<u8>, bytes_read: &mut us
 }
 
 fn write_vector_to_file(filename: &str, input: Vec<u8>) -> usize {
+    let path = Path::new(filename);
+    let display = path.display();
+    let mut file = match OpenOptions::new().write(true).create(true).open(filename) {
+        Err(e) => panic!("couldn't create {}: {}", display, e),
+        Ok(file) => file,
+    };
+    match file.write_all(&input) {
+        Err(e) => {
+            panic!("couldn't write to {}: {}", display, e)
+        },
+        Ok(_) => println!("write successful")
+    };
     return 0 as usize;
 }
 
 fn main() {
 
+    let args: Vec<String> = env::args().collect();
+
+    //parse args
+    let mut filename = "input";
+    if args.len() > 1 {
+        filename = &args[1];
+    }
+    let print_decoded = false;
     //here we read the whole file into memory
     let mut file_input_buffer: Vec<u8> = Vec::new();
     //this is how many bytes were read during file read
     let mut bytes_read : usize = 0;
     //read file into a vector. store the size in bytes read.
-    read_file_to_memory("input", &mut file_input_buffer, &mut bytes_read);
+    read_file_to_memory(filename, &mut file_input_buffer, &mut bytes_read);
 
     //this is where we are going to write our encoded output
     let mut compressed_data_buffer: Vec<u16> = Vec::new();
@@ -113,6 +137,7 @@ fn main() {
         let duration = start.to(end);
 
         //println!("{:?}", output);
+        println!("compression complete\ndictionary size: {}", dictionary.len());
         println!("compressed output bytes: {}", compressed_data_buffer.len()*2);
         let compression_ratio: f32 = 2.0*compressed_data_buffer.len() as f32/bytes_read as f32 * 100.0;
         println!("Compression ratio: {}%", compression_ratio);
@@ -141,7 +166,7 @@ fn main() {
         for x in 0..compressed_data_buffer.len() {
             code = compressed_data_buffer[x].clone();
 
-            if (dictionary.contains_key(&code) == false) {
+            if dictionary.contains_key(&code) == false {
                 let mut temp = previous.clone();
                 temp.push(previous[0]);
                 dictionary.insert(code, temp);
@@ -153,12 +178,18 @@ fn main() {
                 let mut combined_string: Vec<u8> = previous.clone();
                 combined_string.push(dictionary[&(code)][0]);
                 dictionary.insert(next_code, combined_string);
+                //println!("{}",next_code);
+
                 next_code+=1;
             }
             previous = dictionary[&code].clone();
         }
 
         //println!("{:?}",decoded_output);
-        println!("{:?}", str::from_utf8(decoded_output.as_slice()).unwrap());
+        if (print_decoded) {
+            println!("{:?}", str::from_utf8(decoded_output.as_slice()).unwrap());
+        }
+        println!("writing file");
+        write_vector_to_file("output", decoded_output);
     }
 }
